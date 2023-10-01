f502d694236eb1db4a0747346bd535dd41bf5851
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2023-10-01 21:32:27 +0300

    New homework

---
+++ 02_PF_SEP_2023/05-Arrays - Lab/08-condenseArrayToNumber.js
@@ -0,0 +1,22 @@
+function condenseArrayToNumber(nums) {
+    let condensed = [];
+   
+    if (nums.length <= 1) {
+        console.log(Number(nums));
+        return;
+    };
+
+    while (1 < nums.length) {
+        for (let j = 0; j < nums.length - 1; j++) {
+            condensed[j] = Number(nums[j]) + Number(nums[j + 1]);
+        }
+        nums = condensed;
+        condensed = [];
+    };
+
+    console.log(Number(nums));
+}
+condenseArrayToNumber([2, 10, 3]);
+condenseArrayToNumber([5, 0, 4, 1]);
+condenseArrayToNumber([5, 0, 4, 1, 2]);
+condenseArrayToNumber([1]);

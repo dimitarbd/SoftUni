dc17a531f0a903973f688f57990a5f7546106a7f
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2023-10-01 13:03:39 +0300

    New homework

--- 02_PF_SEP_2023/04-Data Types and Variables - Exercise/09-gladiatorExpenses.js
+++ 02_PF_SEP_2023/04-Data Types and Variables - Exercise/09-gladiatorExpenses.js
@@ -4,28 +4,28 @@ function gladiatorExpenses(lostFights, helmetPrice, swordPrice, shieldPrice, arm
     let shield = 0;
     let armor = 0;
 
-    // for (let i = 1; i <= lostFights; i++) {
-    //     let breaks = 0;
-    //     if (i % 2 == 0) {
-    //         helmet++;
-    //         breaks++;
-    //     }
-    //     if (i % 3 == 0) {
-    //         sword++;
-    //         breaks++;
-    //     }
-    //     if (breaks == 2) {
-    //         shield++;
-    //     }
-    //     if (shield > 0 && shield % 2 == 0) {
-    //         armor++;
-    //     }
-    // }
+    for (let i = 1; i <= lostFights; i++) {
+        let breaks = 0;
+        if (i % 2 == 0) {
+            helmet++;
+            breaks++;
+        }
+        if (i % 3 == 0) {
+            sword++;
+            breaks++;
+        }
+        if (breaks == 2) {
+            shield++;
+            if (shield > 0 && shield % 2 == 0) {
+                armor++;
+            }
+        }
+    }
 
-    helmet = Math.floor(lostFights/2);
-    sword = Math.floor(lostFights/3);
-    shield = Math.floor(lostFights/6);
-    armor = Math.floor(lostFights/12);
+    // helmet = Math.floor(lostFights/2);
+    // sword = Math.floor(lostFights/3);
+    // shield = Math.floor(lostFights/6);
+    // armor = Math.floor(lostFights/12);
     
     let result = helmet * helmetPrice + sword * swordPrice + shield * shieldPrice + armor * armorPrice;
     console.log(`Gladiator expenses: ${result.toFixed(2)} aureus`);

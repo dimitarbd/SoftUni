7f02e6ade2a112499b1b3af2dcc31dd19454488e
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2023-10-02 23:45:21 +0300

    Update and new homework

--- 02_PF_SEP_2023/06-Arrays - Exercise/04-arrayRotation.js
+++ 02_PF_SEP_2023/06-Arrays - Exercise/04-arrayRotation.js
@@ -1,6 +1,5 @@
 function arrayRotation(arr, n) {
     
-
     for (let i = 1; i <= n; i++){
         let arrN = [];
         for (let j = 1; j < arr.length; j++) {
@@ -11,6 +10,7 @@ function arrayRotation(arr, n) {
         arr = arrN;
     }
 console.log(arr.join(' '));
+
 }
 arrayRotation([51, 47, 32, 61, 21], 2);
 arrayRotation([32, 21, 61, 1], 4);

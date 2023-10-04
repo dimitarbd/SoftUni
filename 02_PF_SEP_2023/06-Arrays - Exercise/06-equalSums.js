3db4fc658b8a32251bac982750f7744df9992e86
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2023-10-04 21:28:35 +0300

    Update 06-equalSums.js

--- 02_PF_SEP_2023/06-Arrays - Exercise/06-equalSums.js
+++ 02_PF_SEP_2023/06-Arrays - Exercise/06-equalSums.js
@@ -1,29 +1,26 @@
 function equalSums(arr) {
-    let sumLeft = 0;
-    let sumRight = 0;
-    let isTrue = false;
-
+    let result = 'no';
+    
     for (let i = 0; i < arr.length; i++) {
-        sumLeft = 0;
-        sumRight = 0;
-        for (let j = i+1; j < arr.length; j++) {
-            sumLeft += arr[j - (i + 1)];
-            sumRight += arr[j]; 
+        let sumLeft = 0;
+        let sumRight = 0;
+        
+        for (let j = i + 1; j < arr.length; j++) {
+                sumRight += arr[j];
+            }
+            for (let k = 0; k < i; k++) {
+                sumLeft += arr[k];
+            }
+            if (sumLeft == sumRight) {
+                result = i;
+                break;
+            }
         }
-        if (sumLeft == sumRight) {
-        console.log(i);
-        isTrue = true;
-        break;
-        }
-    }
-    if (!isTrue) {
-        console.log('no');
-    }
-    // console.log(arr[0]);
+        console.log(result);
+    
 }
 equalSums([1, 2, 3, 3]);
-// equalSums([1, 2]);
-// equalSums([1]);
-// equalSums([1, 2, 3]);
-// equalSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);
-               
+equalSums([1, 2]);
+equalSums([1]);
+equalSums([1, 2, 3]);
+equalSums([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);

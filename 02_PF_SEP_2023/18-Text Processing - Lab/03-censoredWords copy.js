8c0c19c16beca070a49e35ffa7c81bfec94992fd
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2023-11-11 11:23:48 +0200

    Create 04-countStringOccurrences.js

--- 02_PF_SEP_2023/18-Text Processing - Lab/03-censoredWords copy.js
+++ 02_PF_SEP_2023/18-Text Processing - Lab/03-censoredWords copy.js
@@ -1,7 +1,8 @@
 function censoredWords(text,str) {
-   
+
    let censored = text.split(str).join('*'. repeat(str.length));
    console.log(censored);
+   
 }
 
 censoredWords('A small sentence with some words', 'small');

49d87e97fe52116dcc8ed9fd8a3788ca79f3232b
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2023-11-28 21:46:10 +0200

    Update 03-p!rates.js

--- 02_PF_SEP_2023/22-Final Exam Preparation -2/03-p!rates.js
+++ 02_PF_SEP_2023/22-Final Exam Preparation -2/03-p!rates.js
@@ -15,15 +15,58 @@ function pirates(arr) {
             pirates[city] = town;
         }
     }
+    arr.shift();
+    let tokens = arr.shift().split('=>');
+
+    while (tokens[0] != "End") {
+        if (tokens[0] == 'Plunder') {
+            let [command, town, people, gold] = tokens;
+            console.log(`${town} plundered! ${gold} gold stolen, ${people} citizens killed.`);
+            let currTown = pirates[town];
+            currTown.people -= people;
+            currTown.gold -= gold;
+            
+            if (currTown.people <= 0 || currTown.gold <= 0) {
+                delete pirates[town];
+                console.log(`${town} has been wiped off the map!`);
+            }
+        } else {
+            let [command, town, gold] = tokens;
+            gold = Number(gold);
+            
+            if (gold < 0) {
+                console.log(`Gold added cannot be a negative number!`);
+            } else {
+                let prosperTown = pirates[town];
+                prosperTown.gold += gold;
+                console.log(`${gold} gold added to the city treasury. ${town} now has ${prosperTown.gold} gold.`);
+            }
+        }
+        tokens = arr.shift().split('=>');
+    }
+    // let counter = 0;
+    // Object.entries(pirates).forEach((x) => counter++);
+    let settlements = Object.entries(pirates);
+    
+    if(settlements.length == 0) {
+        console.log(`Ahoy, Captain! All targets have been plundered and destroyed!`);
+    } else {
+        console.log(`Ahoy, Captain! There are ${settlements.length} wealthy settlements to go to:`);
+        for (let name in pirates) {
+            let town = pirates[name];
+
+            console.log(`${name} -> Population: ${town.people} citizens, Gold: ${town.gold} kg`);
+        }
+    }
 }
-// pirates(["Tortuga||345000||1250",
-// "Santo Domingo||240000||630",
-// "Havana||410000||1100",
-// "Sail",
-// "Plunder=>Tortuga=>75000=>380",
-// "Prosper=>Santo Domingo=>180",
-// "End"]);
-// console.log('===============');
+pirates(["Tortuga||345000||1250",
+"Santo Domingo||240000||630",
+"Havana||410000||1100",
+"Sail",
+"Plunder=>Tortuga=>75000=>380",
+"Prosper=>Santo Domingo=>180",
+"End"]);
+console.log('===============');
 pirates(["Nassau||95000||1000",
 "San Juan||930000||1250",
 "Campeche||270000||690",

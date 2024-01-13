80bb6658c4cbdd6568105948eb5311c4d8acd31c
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-01-13 17:20:42 +0200

    Create 07-cookingByNumbers.js

--- 03-PA_Jan_2024/02-Syntax, Functions and Statements - Exercise/05-timeToWalk.js
+++ 03-PA_Jan_2024/02-Syntax, Functions and Statements - Exercise/05-timeToWalk.js
@@ -3,13 +3,13 @@ function timeToWalk(steps, footprint, speed) {
     let speedMeterInSec = speed / 3.6;
     let time = distace / speedMeterInSec;
     let rest = Math.floor(distace / 500);
-    time += rest*60;
+    time += rest * 60;
 
     let sec = Math.round(time % 60);
-    let min = Math.floor(time/60);
-    let hours = Math.floor(min/60);
-    min -= hours*60;
-    let hoursToPrint = hours <10 ? `0${hours}` : `${hours}`;
+    let min = Math.floor(time / 60);
+    let hours = Math.floor(min / 60);
+    min -= hours * 60;
+    let hoursToPrint = hours < 10 ? `0${hours}` : `${hours}`;
     let minToPrint = min < 10 ? `0${min}` : `${min}`;
     let secToPrint = sec < 10 ? `0${sec}` : `${sec}`;
 
---
+++ 03-PA_Jan_2024/02-Syntax, Functions and Statements - Exercise/06-roadRadar.js
@@ -0,0 +1,10 @@
+function roadRadar(speed, area) {
+
+}
+roadRadar(40, 'city');
+console.log('--------------');
+roadRadar(21, 'residential');
+console.log('--------------');
+roadRadar(120, 'interstate');
+console.log('--------------');
+roadRadar(200, 'motorway');

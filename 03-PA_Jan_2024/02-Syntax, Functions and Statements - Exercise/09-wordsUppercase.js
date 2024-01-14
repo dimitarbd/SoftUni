fef55d3e4ec4220725845d05b0af099d799ad563
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-01-14 11:18:44 +0200

    Update 06-roadRadar.js

--- 03-PA_Jan_2024/02-Syntax, Functions and Statements - Exercise/09-wordsUppercase.js
+++ 03-PA_Jan_2024/02-Syntax, Functions and Statements - Exercise/09-wordsUppercase.js
@@ -1,12 +1,13 @@
 function wordsUppercase(str) {
     console.log(
         str.split(/[^a-zA-Z0-9]+/)
-        .join(' ')
-        .trim()
-        .split(' ')
-        .map(x => x.toUpperCase())
-        .join(', ')
-        );
+            .filter(x => x !== '')
+            // .join(' ')
+            // .trim()
+            // .split(' ')
+            .map(x => x.toUpperCase())
+            .join(', ')
+    );
 }
 wordsUppercase('Hi, how are you?');
 console.log('------------------');

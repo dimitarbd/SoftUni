221e62395d67406a8e217677a075639a3a9b23f7
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-01-14 22:40:58 +0200

    Create 10-aggregateElements.js

--- 03-PA_Jan_2024/01-Syntax, Functions and Statements - Lab/09-squareOfStars.js
+++ 03-PA_Jan_2024/01-Syntax, Functions and Statements - Lab/09-squareOfStars.js
@@ -1,7 +1,7 @@
 function squareOfStars(n) {
     if (n > 0) {
         for (let i = 0; i < n; i++) {
-            console.log("*".repeat(n));
+            console.log("* ".repeat(n));
         }
     } else {
         for (let i = 0; i < 5; i++) {

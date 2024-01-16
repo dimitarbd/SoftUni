21b0ee8bb48e7426439e1c3a5acb649f1273f428
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-01-16 22:10:05 +0200

    Create 01-printWithDelimiter.js

--- 03-PA_Jan_2024/01-Syntax, Functions and Statements - Lab/10-aggregateElements.js
+++ 03-PA_Jan_2024/01-Syntax, Functions and Statements - Lab/10-aggregateElements.js
@@ -1,8 +1,10 @@
 function aggregateElements(arr) {
     let sum = arr.reduce((acc, el) => acc + el);
     console.log(sum);
+
     let sumInverse = arr.reduce((acc, val) =>  acc + 1 / val, 0);
     console.log(sumInverse);
+    
     console.log(arr.join(''));
 }
 aggregateElements([1, 2, 3]);

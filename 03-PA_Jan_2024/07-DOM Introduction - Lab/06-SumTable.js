828c912e7b52a2781adc810742daeaf211da07ce
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-01-23 20:21:27 +0200

    Create subtract.js

--- 03-PA_Jan_2024/07-DOM Introduction - Lab/06-SumTable.js
+++ 03-PA_Jan_2024/07-DOM Introduction - Lab/06-SumTable.js
@@ -2,7 +2,7 @@ function sumTable() {
     let table = document.querySelectorAll("table tr");
     let total = 0;
 
-    for (let i = 1; i < table.length-1; i++) {
+    for (let i = 1; i < table.length; i++) {
         let cols = table[i].children;
         let cost = cols[cols.length - 1].textContent;
         total += Number(cost)
---
+++ 03-PA_Jan_2024/08-DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,12 @@
+function subtract() {
+    let firstNum = document.getElementById("firstNumber");
+    let secondNum = document.getElementById("secondNumber");
+    let resultRef = document.getElementById('result');
+
+    let firstNumber = Number(firstNum.value);
+    let secondNumber = Number(secondNum.value);
+
+    let result = firstNumber - secondNumber;
+    
+    resultRef.textContent = result;
+}

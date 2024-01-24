250b2e6e7b9874b4e218022f0882617214971156
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-01-24 21:21:52 +0200

    Create 02. Delete-From-Table

--- 03-PA_Jan_2024/09-DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
+++ 03-PA_Jan_2024/09-DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -1,7 +1,11 @@
 function addItem() {
     let input = document.getElementById("newItemText");
+    if (input.value.length == 0) {
+        return;
+    }
     let liElement = document.createElement("li");
     liElement.textContent = input.value;
+
 
     let list = document.getElementById("items");
     list.appendChild(liElement);

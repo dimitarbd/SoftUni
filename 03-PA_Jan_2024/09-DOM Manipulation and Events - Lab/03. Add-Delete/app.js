70a3f12ca819742e50c0a3efa8cd69e0d83039bd
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-01-24 23:45:19 +0200

    Create 03. Add-Delete

--- 03-PA_Jan_2024/09-DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ 03-PA_Jan_2024/09-DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,27 @@
 function addItem() {
-    //TODO...
+    let input = document.getElementById("newItemText");
+    if (input.value.length == 0) {
+        return;
+    }
+
+    let liElement = document.createElement("li");
+    liElement.textContent = input.value;
+
+    let deleteBtn = document.createElement('a');
+    deleteBtn.textContent = "[Delete]";
+    deleteBtn.href = '#';
+    liElement.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', onDelete)
+
+    let list = document.getElementById("items");
+    list.appendChild(liElement);
+
+    input.value = "";
+
+}
+
+function onDelete(event) {
+    let deleteBtn = event.target;
+    let liElement = deleteBtn.parentElement;
+    liElement.remove();
 }

0f539cc019767d615f0d38ec84eb35596a19bfe8
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-02-04 01:07:13 +0200

    Update app.js

--- 03-PA_Jan_2024/14-Unit Testing and Error Handling - Exercise/05.Notification/app.js
+++ 03-PA_Jan_2024/14-Unit Testing and Error Handling - Exercise/05.Notification/app.js
@@ -1,4 +1,18 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  let divEl = document.getElementById("notification");
+ 
+  let msg = document.createElement("p");
+      msg.textContent = message;
+      divEl.appendChild(msg);
+      divEl.style.display = 'block'
+
+  divEl.addEventListener("click", hideDiv);
+  
+
+  function hideDiv() {
+    divEl.style.display = 'none';
+    msg.textContent = ""
+  }
+ 
 }
+

6f61c4c42c0af554bf996635f9011179375de781
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-02-21 21:03:17 +0200

    Update app1.js

--- 03-PA_Jan_2024/20-Exam Preparation 2/01. Ski lift-Resources/app1.js
+++ 03-PA_Jan_2024/20-Exam Preparation 2/01. Ski lift-Resources/app1.js
@@ -1,51 +1,74 @@
 window.addEventListener('load', solve);
 
 function solve() {
-let formRef = document.querySelector("form");
-formRef.addEventListener("submit", onSubmit);
+    let formRef = document.querySelector("form");
+    formRef.addEventListener("submit", onSubmit);
 
-let firstNameRef = document.getElementById("first-name");
-let lastNameRef = document.getElementById("last-name");
-let peopleCountRef = document.getElementById("people-count");
-let formDateRef = document.getElementById("from-date");
-let daysCountRef = document.getElementById("days-count");
+    let firstNameRef = document.getElementById("first-name");
+    let lastNameRef = document.getElementById("last-name");
+    let peopleCountRef = document.getElementById("people-count");
+    let fromDateRef = document.getElementById("from-date");
+    let daysCountRef = document.getElementById("days-count");
+    let infoTicketUl = document.getElementById("info-ticket")
+    let nextBtnRef = document.getElementById("next-btn")
 
-let infoTicket = document.querySelector(".ticket-info-list");
-
-
-function onSubmit(e) {
-    e.preventDefault();
-    let firstName = firstNameRef.value;
-    let lastName = lastNameRef.value;
-    let peopleCount = peopleCountRef.value;
-    let formDate = formDateRef.value;
-    let daysCount = daysCountRef.value;
-    
+    let infoTicket = document.querySelector(".ticket-info-list");
 
 
-   if (!firstName || !lastName || !peopleCount|| !formDateRef || !daysCount) {
-    return
-   }
+    function onSubmit(e) {
+        e.preventDefault();
+        let firstName = firstNameRef.value;
+        let lastName = lastNameRef.value;
+        let peopleCount = peopleCountRef.value;
+        let fromDate = fromDateRef.value;
+        let daysCount = daysCountRef.value;
 
-   let ticket = createTicket(firstName, lastName, peopleCount, daysCount);
 
-   infoTicket.appendChild(ticket);
 
-   function createTicket(firstName, lastName, peopleCount, daysCount) {
-    let li = document.createElement("li");
-    li.classList.add("ticket");
-    li.innerHTML += "<article>";
-    li.innerHTML += `<h3>Name: ${firstName} ${lastName}</h3>`;
-    li.innerHTML += `<p>Form date: ${formDate}</p>`;
-    li.innerHTML += `<p>For ${daysCount} days</p>`;
-    li.innerHTML += `<p>For ${peopleCount} people</p>`
-    li.innerHTML += "</article>";
-    
-    return li;
-   }
+        if (!firstName || !lastName || !peopleCount || !fromDate || !daysCount) {
+            return
+        }
 
-}
-    
+        let ticket = createTicket(firstName, lastName, peopleCount, fromDate, daysCount);
+
+        infoTicketUl.appendChild(ticket);
+        toggleBtnNext();
+        formRef.reset();
+    }
+
+    function createTicket(firstName, lastName, peopleCount, fromDate, daysCount) {
+        let li = document.createElement("li");
+        li.classList.add("ticket");
+        li.innerHTML += "<article>";
+        li.innerHTML += `<h3>Name: ${firstName} ${lastName}</h3>`;
+        li.innerHTML += `<p>Form date: ${fromDate}</p>`;
+        li.innerHTML += `<p>For ${daysCount} days</p>`;
+        li.innerHTML += `<p>For ${peopleCount} people</p>`
+        li.innerHTML += "</article>";
+
+        let editBtn = createBtn("edit-btn", "Edit");
+        let continueBtn = createBtn("continue-btn", "Continue");
+        li.appendChild(editBtn);
+        li.appendChild(continueBtn);
+
+        return li;
+    }
+
+    function toggleBtnNext() {
+
+        nextBtnRef.disabled = !nextBtnRef.disabled;
+    }
+
+    function createBtn(classes, text) {
+        let btn = document.createElement("button");
+        btn.classList.add(classes);
+        btn.textContent = text;
+        return btn;
+    }
+
+
+
+
 }
 
 

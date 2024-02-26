deeaee8493e876d4a5d8db2134a260683df65c05
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-02-26 22:07:07 +0200

    Update app.js

--- 03-PA_Jan_2024/02-Aplications/02.JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
+++ 03-PA_Jan_2024/02-Aplications/02.JS-Applications-Asynchronous-Programming-Lab-Resources/03.Gighub-Commits/app.js
@@ -1,4 +1,37 @@
+let list = document.getElementById('commits')
+
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.getElementById("username").value;
+    let repo = document.getElementById("repo").value;
+
+    let url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    fetch(url)
+        .then(onHeaders)
+        .then(displayCommits)
+        .catch(onError);
+}
+
+function onHeaders(responce) {
+    if (!responce.ok) {
+        throw "Error";
+    }
+
+    return responce.json();
+}
+
+function displayCommits(data) {
+    
+    list.replaceChildren(...data.map(createListItem));
+}
+
+function onError(error) {
+    console.log(error);
+}
+
+function createListItem({ commit: { author: { name }, message } }) {
+    let item = document.createElement('li');
+    item.textContent = `${name}: ${message}`
+
+    return item;
 }

f33b2cb42b44c4acb351c579d116addc65d4cbde
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-03-02 21:23:07 +0200

    Update app.js

--- 03-PA_Jan_2024/02-Aplications/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
+++ 03-PA_Jan_2024/02-Aplications/02.JS-Applications-Asynchronous-Programming-Exercise-Resources/03.Forecaster/app.js
@@ -1,5 +1,28 @@
 function attachEvents() {
-    console.log("TODO...");
+    document.getElementById("submit").addEventListener("click", getWeather);
+    let forecastSectionRef =document.getElementById("forecast");
+    let locationInputRef = document.getElementById("location");
+    let currentRef = document.getElementById("current");
+    let upcomingRef = document.getElementById("upcoming");
+
+    let baseLocationURL = "http://localhost:3030/jsonstore/forecaster/locations";
+    let todayURL = "http://localhost:3030/jsonstore/forecaster/today/";
+
+
+    async function getWeather(e){
+        let userInput = locationInputRef.value;
+        forecastSectionRef.style.display = "block";
+        let locationResponse = await fetch(baseLocationURL);
+        let locationData = await locationResponse.json() ;
+        let currentLocationData = locationData.find(x=> x.name == userInput)
+        fillTodayData(currentLocationData.code)
+    }
+
+    async function fillTodayData (code) {
+        let response = await fetch(todayURL + code);
+        let data = await response.json();
+        debugger
+    }
 }
 
 attachEvents();

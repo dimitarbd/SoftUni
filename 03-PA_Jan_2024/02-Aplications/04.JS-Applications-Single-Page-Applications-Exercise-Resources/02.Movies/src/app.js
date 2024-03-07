709deb02cf8e80d06d9b47655d5a78da89eb35f5
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-03-07 22:03:00 +0200

    Create register.js

--- 03-PA_Jan_2024/02-Aplications/04.JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/app.js
+++ 03-PA_Jan_2024/02-Aplications/04.JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/app.js
@@ -1,8 +1,20 @@
+import {showRegisterView} from "./register.js"
+
 document.querySelectorAll("section").forEach(section => section.style.display="none");
 let userNav = document.querySelectorAll("li.user");
 let guestNav = document.querySelectorAll("li.guest");
+document.querySelector("nav").addEventListener("click", onNavigate);
 
 let userData = JSON.parse(sessionStorage.getItem("userData"));
+
+function onNavigate (e) {
+    if (e.target.tagName !== "A" || e.target.href) {
+        return
+    }
+    let url = new URL(e.target.href);
+    let path = url.pathname;
+
+}
 
 function updateNav() {
     if (userData) {

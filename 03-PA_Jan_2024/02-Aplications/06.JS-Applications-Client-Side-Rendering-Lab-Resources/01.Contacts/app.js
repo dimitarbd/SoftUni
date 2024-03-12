db428e63827f2d0c5a6490b631a1002c8b8414d1
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-03-12 20:51:32 +0200

    Update app.js

--- 03-PA_Jan_2024/02-Aplications/06.JS-Applications-Client-Side-Rendering-Lab-Resources/01.Contacts/app.js
+++ 03-PA_Jan_2024/02-Aplications/06.JS-Applications-Client-Side-Rendering-Lab-Resources/01.Contacts/app.js
@@ -3,20 +3,30 @@ import { contacts } from "./contacts.js";
 
 const root = document.getElementById('contacts');
 
-const cardTemplate = () => html`
+root.addEventListener("click", onClick);
+
+const cardTemplate = (contact) => html`
 <div class="contact card">
             <div>
                 <i class="far fa-user-circle gravatar"></i>
             </div>
             <div class="info">
-                <h2>Name: John</h2>
+                <h2>Name: ${contact.name}</h2>
                 <button class="detailsBtn">Details</button>
-                <div class="details" id="1">
-                    <p>Phone number: 0847759632</p>
-                    <p>Email: john@john.com</p>
+                <div class="details" id=${contact.id}>
+                    <p>Phone number: ${contact.phoneNumber}</p>
+                    <p>Email: ${contact.email}</p>
                 </div>
             </div>
         </div>
 `
 
-render(cardTemplate(), root)
+render(contacts.map(cardTemplate), root);
+
+function onClick(e) {
+    if (e.target.classList.contains("detailsBtn")) {
+        const details = e.target.parentElement.querySelector(".details");
+        const isHidden = details.style.display !== 'block';
+        details.style.display = isHidden ? 'block' : 'none'
+    }
+}

e0d294e7feeaa97ed23f9fcacf6e2599a94e2dc4
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-03-13 22:54:29 +0200

    Create 03.Search-in-List

--- 03-PA_Jan_2024/02-Aplications/06.JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
+++ 03-PA_Jan_2024/02-Aplications/06.JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
@@ -1,3 +1,35 @@
-function search() {
-   // TODO
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { towns } from "./towns.js"; 
+
+document.querySelector("button").addEventListener("click", search);
+
+let townsRoot = document.getElementById("towns");
+let resultRoot = document.getElementById("result");
+let inputRef = document.getElementById("searchText");
+
+
+update();
+
+function update(match) {
+    render(ulTemplate(towns, match),townsRoot)
 }
+
+function ulTemplate(towns, match)  {
+   return html `<ul>${towns.map(town => createTemplate(town, match?.includes(town)))}</ul>`
+};
+
+function createTemplate (town, isActive) {
+   return html`<li class=${isActive ? "active" : ""}>${town}</li>`
+};
+
+function search() {
+   let searchText = inputRef.value;
+   let match = towns.filter(town => town.includes(searchText));
+   update(match);
+   renderMatchCount(match.length);
+}
+
+function renderMatchCount(count) {
+   render(html`${count} matches found`, resultRoot)
+}
+

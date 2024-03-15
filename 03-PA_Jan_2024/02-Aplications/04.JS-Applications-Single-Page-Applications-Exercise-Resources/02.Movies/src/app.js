6627d5634031292861a3684f593f9e814afa3d1a
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-03-15 23:47:38 +0200

    Create 02.Movies

---
+++ 03-PA_Jan_2024/02-Aplications/04.JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/app.js
@@ -0,0 +1,14 @@
+const host = 'http://localhost:3030'
+
+async function request(method, url, data) {
+    const options = {
+        method, 
+        headers: {}
+    }
+
+    if(data != undefined) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+        
+    }
+}
---
+++ 03-PA_Jan_2024/02-Aplications/04.JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/util.js
@@ -0,0 +1,3 @@
+export function saveUserData(data) {
+    localStorage.setItem('user')
+}

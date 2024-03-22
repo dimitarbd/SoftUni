a270513ee6eb6647457c709076c2d2ef570a1ddc
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-03-22 23:44:32 +0200

    Create 01.Team-manager

---
+++ 03-PA_Jan_2024/02-Aplications/08.JS-Applications-Modular-Applications-Exercise-Resources/01.Team-Manager/src/api/requester.js
@@ -0,0 +1,38 @@
+async function requester(method, url, data) {
+    let option = {
+        method
+    }
+    if (data) {
+        option.headers = {
+            "Content-Type": "appliction/json"
+        }
+        option.body = JSON.stringify(data);
+    };
+    try {
+        let response = await fetch(url, option);
+        if (!response) {
+            let error = await response.json();
+            throw new Error(error.message);
+        }
+
+        if (response.status == 204) {
+            return response;
+        }
+        let responseData = await response.json();
+
+    } catch (error) {
+        return alert(error);
+    }
+}
+
+async function get(url) { return await requester("GET", url) };
+async function post(url, data) { return await requester("POST", url, data) };
+async function put(url, data) { return await requester("PUT", url, data) };
+async function del(url){ return await requester("DELETE", url) };
+
+export let api = {
+    get,
+    post,
+    put,
+    del
+}

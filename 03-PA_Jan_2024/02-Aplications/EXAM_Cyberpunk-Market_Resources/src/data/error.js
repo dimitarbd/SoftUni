5541b9f621c2c97eacf4d8a7774cd42ab30b50a8
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-03-30 21:15:31 +0200

    Create Exam

---
+++ 03-PA_Jan_2024/02-Aplications/EXAM_Cyberpunk-Market_Resources/src/data/error.js
@@ -0,0 +1,19 @@
+
+
+export function showError (str) {
+    const errorBox = document.querySelector('#errorBox');
+    const errSpan = errorBox.querySelector('.msg');
+
+    errSpan.textContent = str;
+    errorBox.style.display = 'block';
+    setTimeout (() => {
+        errorBox.style.display = 'none';
+    }, 3000);
+}
+
+
+/* <section id="notifications">
+<div id="errorBox" class="notification">
+  <span class="msg">MESSAGE</span>
+</div>
+</section> */

a9dbe0459fe886269988abaf7c1125802585dd0e
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-07 21:22:41 +0300

    Update app.js

--- 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/app.js
+++ 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/app.js
@@ -1,10 +1,12 @@
 import page from '@page/page.mjs';
 import { html, render} from '@lit-html/lit-html.js';
 import { showExample } from '@src/views/example.js';
+import { session } from './middleware/session.js';
 
 import * as api from '@src/data/users.js';
 window['api'] = api;
 
+page(session());
 page('/', showExample);
 
 page.start();

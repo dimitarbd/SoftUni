3562fab78dd296ba823d44226cbdfd0a40a329ff
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-09 23:36:14 +0300

    Update session.js

--- 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/middleware/session.js
+++ 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/middleware/session.js
@@ -1,14 +1,15 @@
+import { logout } from '@src/data/users';
 import { getUserData } from '@src/util.js';
 
 export function session() {
     document.getElementById("logoutbtn").addEventListener("click", onLogout);
     return function(ctx, next) {
         ctx.user = getUserData();
-        console.error('session');
         next();
     };
 }
 
 function onLogout() {
-    // logout();
+    logout();
+    goTo('/');
 }

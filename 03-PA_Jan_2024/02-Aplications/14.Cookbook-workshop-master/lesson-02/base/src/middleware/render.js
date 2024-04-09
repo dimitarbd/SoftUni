3e0726948b10a34502f92fe5f2a3b956bff3c962
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-09 23:39:51 +0300

    Create render.js

---
+++ 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/middleware/render.js
@@ -0,0 +1,13 @@
+import { html, render as baseRender } from '@lit-html/lit-html.js';
+
+export function renderer(root) {
+    return function(ctx, next) {
+        ctx.render = (templateResult) => baseRender(templateResult, root);
+
+        next();
+    };
+}
+
+function updateUserNav() {
+    
+}
--- 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/middleware/session.js
+++ 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/middleware/session.js
@@ -2,7 +2,7 @@ import { logout } from '@src/data/users';
 import { getUserData } from '@src/util.js';
 
 export function session() {
-    document.getElementById("logoutbtn").addEventListener("click", onLogout);
+    document.getElementById('logoutbtn').addEventListener('click', onLogout);
     return function(ctx, next) {
         ctx.user = getUserData();
         next();

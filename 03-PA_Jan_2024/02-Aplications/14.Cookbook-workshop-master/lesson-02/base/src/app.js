71d747e8f38adf802504828a9ba6dc7e8f307cdf
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-10 22:34:36 +0300

    Update

--- 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/app.js
+++ 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/app.js
@@ -5,11 +5,18 @@ import { session } from './middleware/session.js';
 import { showRegister } from './register.js';
 
 import * as api from '@src/data/users.js';
+import { renderer } from './middleware/render.js';
 window['api'] = api;
 
 page(session());
-page('/', showExample);
+page(renderer(document.querySelector('main')));
+page('/', loading(), preload('recipes'), showCatalog);
 page('/register', showRegister);
+page('/login', showLogin)
 
 page.start();
+
+export function goTo(path) {
+    page.redirect(path);
+}
 

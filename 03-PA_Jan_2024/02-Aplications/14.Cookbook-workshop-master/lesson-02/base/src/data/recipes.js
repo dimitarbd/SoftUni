fba90f2c41825162ee4fd1bd5edc4cb331fabc86
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-12 22:48:41 +0300

    Update recipes.js

--- 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/data/recipes.js
+++ 03-PA_Jan_2024/02-Aplications/14.Cookbook-workshop-master/lesson-02/base/src/data/recipes.js
@@ -1,4 +1,4 @@
-import { get } from './request.js';
+import { get, post } from './request.js';
 
 export async function getAllRecipes() {
     return get(endpoints.recipes);
@@ -8,3 +8,18 @@ export async function getAllRecipes() {
  * @param {string} id
  * @returns {Promise<RecipeModel>}
  */
+
+export async function getRecipeById(id) {
+    return get(endpoints.recipeById + id);
+}
+
+/**
+ * 
+ * @param {RecipeModel} data
+ */
+
+export async function createRecipe(data) {
+    post(endpoints.recipes, data);
+}
+
+

514abd0e1d36d3221c8e84d21b2e618c57f35929
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-21 17:13:53 +0300

    Update

--- 03-PA_Jan_2024/01-Advanced/01-Syntax, Functions and Statements - Lab/e004-stringLength.js
+++ 03-PA_Jan_2024/01-Advanced/01-Syntax, Functions and Statements - Lab/e004-stringLength.js
@@ -1,6 +1,6 @@
 function stringLength(a, b, c) {
     let total = a.length + b.length+ c.length;
-    let avg = Math.floor(total / 3);
+    let avg = total / 3 | 0;
     
     console.log(total);
     console.log(avg);

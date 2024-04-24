c86de66a62342ebacbb672c2d0bf213350de8e0b
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-24 22:44:08 +0300

    Update

--- 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e002-greatestCommonDivisor.js
+++ 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e002-greatestCommonDivisor.js
@@ -1,3 +1,14 @@
 function greatestCommonDivisor(a, b) {
+    let variable = a % b;
+
+    while (variable !==0) {
+        a = b;
+        b = variable;
+        variable = a % b;
+    }
+
+    console.log(b);
 
 }
+
+greatestCommonDivisor(15, 5);

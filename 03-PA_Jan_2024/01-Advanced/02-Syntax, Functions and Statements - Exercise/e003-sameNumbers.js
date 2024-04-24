28c213d5d37bf299ea7346b6e81ce979e0b9c44f
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-24 23:10:34 +0300

    Update

--- 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e003-sameNumbers.js
+++ 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e003-sameNumbers.js
@@ -1,3 +1,16 @@
 function sameNumbers(num) {
+    let isSame = true;
+    let sum = 0;
+    let numAsString = num.toString();
 
+    for (let i= 0; i < numAsString.length; i++) {
+        if (numAsString[i] !== numAsString[i-1]) {
+            isSame = false;
+        }
+    }
+    console.log(isSame);
 }
+
+sameNumbers(2222222);
+console.log('=========');
+sameNumbers(1234);

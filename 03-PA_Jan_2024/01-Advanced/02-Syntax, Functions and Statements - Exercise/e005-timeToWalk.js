1c5a81307ccacbe89c7d063ff159aa1b229b3648
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-04-30 15:58:38 +0300

    Update

--- 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e005-timeToWalk.js
+++ 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e005-timeToWalk.js
@@ -1,5 +1,16 @@
-function timeToWalk(input) {
+function timeToWalk(steps, footprint, speed) {
+    let distace = Number(steps) * Number(footprint);
+    let speedMeterInSec = Number(speed)/3.6;
+    let time = distace / speedMeterInSec;
+    let rest = Math.floor(distace / 500);
 
+    let min = Math.floor(time / 60);
+    
+    let hours = 0;
+    if (min > 60) {
+        hours = Math.floor(min/60);
+        min = min % 60;
+    }
 }
 
 timeToWalk(4000, 0.60, 5);

08eeec91cc8df83fd84f23c0075c9a529ae838ec
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-05-01 21:32:24 +0300

    Update

--- 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e007-cookingByNumbers.js
+++ 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e007-cookingByNumbers.js
@@ -1,3 +1,16 @@
-function cookingByNumbers(numAsString, op, op1, op2, op3, op4, op5) {
-    
+function cookingByNumbers(numAsString, op1, op2, op3, op4, op5) {
+    let num = Number(numAsString);
+    let op = [op1, op2, op3, op4, op5];
+
+    for(let i =0; i <op.length; i++) {
+        switch(op[i]) {
+            case 'chop': num /= 2; break;
+            case 'dice': num = Math.sqrt(num); break;
+            case 'spice': num += 1; break;
+            case 'bake': num *= 3; break;
+            case 'fillet': num *= 0.80; break;
+        }
+        
+    }
+
 }

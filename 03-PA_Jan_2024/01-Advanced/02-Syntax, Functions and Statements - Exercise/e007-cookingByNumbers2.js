8bac878fbcb5a02ef207fc8d3113394bab4b6fe7
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-05-01 21:42:10 +0300

    Update

--- 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e007-cookingByNumbers2.js
+++ 03-PA_Jan_2024/01-Advanced/02-Syntax, Functions and Statements - Exercise/e007-cookingByNumbers2.js
@@ -1,5 +1,11 @@
 function cookingByNumbers(numAsString, op1, op2, op3, op4, op5) {
-    
+    let operationEnum = {
+        chop: (num) => num /2,
+        dice: (num) => Math.sqrt(num),
+        spice: (num) => num +1,
+        bake: (num) => num +3,
+        fillet: (num) => num * 0.80
+    };
 }
 
 cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');

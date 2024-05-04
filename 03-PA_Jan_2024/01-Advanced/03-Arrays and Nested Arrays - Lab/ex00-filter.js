cae45704f8ae17372fb0bb334dbc61ee9a59e736
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-05-04 23:42:40 +0300

    Update

--- 03-PA_Jan_2024/01-Advanced/03-Arrays and Nested Arrays - Lab/ex00-filter.js
+++ 03-PA_Jan_2024/01-Advanced/03-Arrays and Nested Arrays - Lab/ex00-filter.js
@@ -1,7 +1,13 @@
 function solve(arr) {
     const filtered = arr.filter((c, i) => i % 2);
-
     console.log(filtered);
+    const doubled =filtered.map(e => e * 2);
+    console.log(doubled);
+    doubled.reverse();
+    console.log(doubled);
+    const result = doubled.join(' ');
+    console.log(result);
+
 }
 
 solve([10, 15, 20, 25]);

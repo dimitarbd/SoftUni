554a124f3d3c58637fcac86c499b09e745877a97
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-05-04 23:48:47 +0300

    Update

--- 03-PA_Jan_2024/01-Advanced/12-Advanced Functions - Exercise/01-sortArray.js
+++ 03-PA_Jan_2024/01-Advanced/12-Advanced Functions - Exercise/01-sortArray.js
@@ -1,6 +1,6 @@
 function sortArray(arr, op) {
 
-    return op === "asc" ? arr.sort((a, b) => a - b) : arr.sort((a, b) => b - a);
+    return op === 'asc' ? arr.sort((a, b) => a - b) : arr.sort((a, b) => b - a);
 
 }
 console.log(sortArray([14, 7, 17, 6, 8], 'asc'));

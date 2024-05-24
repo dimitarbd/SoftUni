90cddb49afc9ef6d7d4a03a04bd80dd92aa98083
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-05-24 21:50:11 +0300

    Update

--- 04-WebMay2024/01-BackEnd/03-Workshop-ExpressJS/src/index.js
+++ 04-WebMay2024/01-BackEnd/03-Workshop-ExpressJS/src/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express!')
+    res.send('Hello, Express!');
+});
+
+app.get('*', (req, res) => {
+    res.status(404);
+    res.send('404 File Not Found');
 });
 
 app.listen(3000, () => {

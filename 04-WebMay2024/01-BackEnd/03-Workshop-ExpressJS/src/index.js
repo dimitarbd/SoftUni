e72ca95088344aa5439067807f512c5544b88d17
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-05-24 23:21:02 +0300

    Update

--- 04-WebMay2024/01-BackEnd/03-Workshop-ExpressJS/src/index.js
+++ 04-WebMay2024/01-BackEnd/03-Workshop-ExpressJS/src/index.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const app = express();
 
+const homeHtml= `
+<h1>Home Page</h1>
+<a href="/">Home</a>
+<a href="/catalog">Catalog</a>
+`;
+
+const catalogHtml= `
+<h1>Catalog Page</h1>
+<a href="/">Home</a>
+<a href="/catalog">Catalog</a>
+`;
+
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.send(homeHtml);
+});
+
+app.get('/catalog', (req, res) => {
+    res.send(catalogHtml);
+});
+
+app.get('/catalog/:productId', (req, res) => {
+    console.log(req.params.productId);
+    res.send(catalogHtml + `<p>Item ID ${req.params.productId}</p>`);
 });
 
 app.get('*', (req, res) => {

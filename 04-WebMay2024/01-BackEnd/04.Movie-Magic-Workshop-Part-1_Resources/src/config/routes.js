a4e3a8af32008687b976d2484692b8513cfcb9cf
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-05-28 22:59:26 +0300

    Update

--- 04-WebMay2024/01-BackEnd/04.Movie-Magic-Workshop-Part-1_Resources/src/config/routes.js
+++ 04-WebMay2024/01-BackEnd/04.Movie-Magic-Workshop-Part-1_Resources/src/config/routes.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 const { home, details } = require('../controllers/catalog');
 const { about } = require('../controllers/about');
+const { createGet } = require('../controllers/movie');
+const { notFound } = require('../controllers/404');
 
 const router = Router();
 
 router.get('/', home);
 router.get('/about', about);
 router.get('/details/:id', details);
+router.get('/create', createGet);
 
+router.get('*', notFound);
 
 module.exports = { router };

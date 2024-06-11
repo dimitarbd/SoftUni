bb3978a25d1d90ab0ee1f9679139766190e94f74
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-06-11 21:24:18 +0300

    Create token.js

--- 04-WebMay2024/01-BackEnd/04.Movie-Magic-Workshop-Part-1_Resources/src/controllers/user.js
+++ 04-WebMay2024/01-BackEnd/04.Movie-Magic-Workshop-Part-1_Resources/src/controllers/user.js
@@ -1,16 +1,26 @@
+const {register} = require('../services/user');
+
 module.exports = {
     registerGet: (req, res) => {
         res.render('register');
     },
-    registerPost: (req, res) => {
+    registerPost: async (req, res) => {
         const { email, password, repass } = req.body;
-        
+
         try {
             if (!email || !password) {
-                throw new Error ('Passwords don\'t match');
+                throw new Error('All fields are required');
             }
-        } catch(err) {
-            res.render('register', { error: err.message });
+            if (!password || !repass) {
+                throw new Error('Passwords don\'t match');
+            }
+
+            const user = await register(email, password);
+
+            res.redirect('/');
+        } catch (err) {
+            res.render('register', { data: { email }, error: err.message });
+            return;
         }
 
         res.redirect('/register');

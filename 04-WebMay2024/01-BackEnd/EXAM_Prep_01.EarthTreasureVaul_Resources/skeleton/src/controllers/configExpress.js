3454696bc8aa2a1eccb99489cd7321cddf1e8e82
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-06-15 13:07:00 +0300

    Update congigExpress.js

--- 04-WebMay2024/01-BackEnd/EXAM_Prep_01.EarthTreasureVaul_Resources/skeleton/src/controllers/configExpress.js
+++ 04-WebMay2024/01-BackEnd/EXAM_Prep_01.EarthTreasureVaul_Resources/skeleton/src/controllers/configExpress.js
@@ -1,8 +1,19 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
 const handlebars = require('express-handlebars');
 
 function configExpress(app) {
     const hbs = handlebars.create({
         extname: 'hbs'
     });
+
+    app.engine('hbs', hbs.engine);
+    app.set('view engine', 'hbs');
+
+    app.use('/static', express.static('static'));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser);
+    // TODO add sessionmiddleware
 }
+
 module.exports = { configExpress };

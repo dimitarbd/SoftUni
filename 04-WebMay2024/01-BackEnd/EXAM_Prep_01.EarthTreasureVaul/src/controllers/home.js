0425d0f49fd897e9e7295ea6b2253603fb3155fd
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-06-17 23:32:01 +0300

    Update

--- 04-WebMay2024/01-BackEnd/EXAM_Prep_01.EarthTreasureVaul/src/controllers/home.js
+++ 04-WebMay2024/01-BackEnd/EXAM_Prep_01.EarthTreasureVaul/src/controllers/home.js
@@ -1,20 +1,13 @@
 const { Router } = require('express');
-const { createToken } = require('../services/jwt');
-const { login } = require('../services/user');
+
 
 // TODO replace with real router according to exam description
 
 const homeRouter = Router();
 
 homeRouter.get('/', async (req, res) => {
-    console.log(req.user);
-
-    //This code creates a token and saves it in a cookie
-    // const result = await login('John', '123456');
-    // const token = createToken(result);
-    // res.cookie('token', token);
-
-    res.render('home');
+    const minerals = [];
+    res.render('home', { minerals });
 });
 
 module.exports = { homeRouter };

1279fd4f1facae6a954e100a14f0c447c17f0dc2
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-06-20 23:17:28 +0300

    Update

--- 04-WebMay2024/01-BackEnd/EXAM_Prep_01.EarthTreasureVaul/src/controllers/home.js
+++ 04-WebMay2024/01-BackEnd/EXAM_Prep_01.EarthTreasureVaul/src/controllers/home.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getRecent } = require('../services/stoneData');
+const { getRecent, getAll, getById } = require('../services/stoneData');
 
 
 // TODO replace with real router according to exam description
@@ -10,5 +10,21 @@ homeRouter.get('/', async (req, res) => {
     const stones = await getRecent();
     res.render('home', { stones });
 });
+
+homeRouter.get('/catalog/:id', async (req, res) => {
+    const stone = await getById (req.params.id);
+
+    
+    if(!stone) {
+        res.render('404');
+        return;
+    }
+    
+    const isOwner = req.user?._id == stone.author.toString();
+    const hasLiked = Boolean(stone.likes.find(1 => req.user?._id == 1.toString()));
+
+    res.render('details', { stone, isOwner, hasLiked });
+});
+
 
 module.exports = { homeRouter };
--- 04-WebMay2024/01-BackEnd/EXAM_Prep_01.EarthTreasureVaul/src/controllers/stone.js
+++ 04-WebMay2024/01-BackEnd/EXAM_Prep_01.EarthTreasureVaul/src/controllers/stone.js
@@ -5,26 +5,117 @@ const { isGuest, isUser } = require('../middlewares/guards');
 const { register, login } = require('../services/user');
 
 const { getRecent } = require('../services/stoneData');
+const { parseError } = require('../util');
+const { createToken } = require('../services/jwt');
+const { getById, create, update, deleteById } = require('../services/stoneData');
 
 // TODO replace with real router according to exam description
 
 const stoneRouter = Router();
 
-stoneRouter.get('/create', isUser(), 
+stoneRouter.get('/create', isUser(),
     async (req, res) => {
-    res.render('create');
-});
+        res.render('create');
+    });
 
-stoneRouter.post('/create', 
+stoneRouter.post('/create',
     body('name').trim().isLength({ min: 2 }).withMessage('name must be at least 2 characters long'),
     body('category').trim().isLength({ min: 3 }).withMessage('category must be at least 3 characters long'),
     body('color').trim().isLength({ min: 2 }).withMessage('color must be at least 2 characters long'),
-    body('image').trim().isURL().isLength({ min: 2 }).withMessage('image must be a valid URL'),
-    body('location').trim().isLength({ min: 5 }).withMessage('location must be between 5 and 15 characters long'),
-    body('formula').trim().isLength({ min: 3 }).withMessage('formula must be between 3 and 30 characters long'),
-    body('description').trim().isLength({ min: 2 }).withMessage('description must be at least 10 characters long'),
+    body('image').trim().isURL({ require_tld: false }).isLength({ min: 2 }).withMessage('image must be a valid URL'),
+    body('location').trim().isLength({ min: 5, max: 15 }).withMessage('location must be between 5 and 15 characters long'),
+    body('formula').trim().isLength({ min: 3, max: 30 }).withMessage('formula must be between 3 and 30 characters long'),
+    body('description').trim().isLength({ min: 10 }).withMessage('description must be at least 10 characters long'),
+
     async (req, res) => {
-    res.render('create');
-});
+        try {
+            const validation = validationResult(req);
+
+            if (validation.errors.length) {
+                throw validation.errors;
+            }
+
+            const result = await create(req.body, req.user._id);
+
+            res.redirect('/catalog');
+
+        } catch (err) {
+            res.render('create', { data: req.body, errors: parseError(err).errors });
+        }
+
+
+    }
+);
+
+
+stoneRouter.get('/edit/:id', isUser(),
+    async (req, res) => {
+        const stone = await getById(req.params.id);
+
+        if (!stone) {
+            res.render('404');
+            return;
+        }
+        const isOwner = req.user._id == stone.author.toString();
+
+        if(!isOwner) {
+            res.redirect('/login');
+            return;
+        }
+        res.render('edit', { data: stone });
+    });
+
+stoneRouter.post('/edit/:id', isUser(),
+    body('name').trim().isLength({ min: 2 }).withMessage('name must be at least 2 characters long'),
+    body('category').trim().isLength({ min: 3 }).withMessage('category must be at least 3 characters long'),
+    body('color').trim().isLength({ min: 2 }).withMessage('color must be at least 2 characters long'),
+    body('image').trim().isURL({ require_tld: false }).isLength({ min: 2 }).withMessage('image must be a valid URL'),
+    body('location').trim().isLength({ min: 5, max: 15 }).withMessage('location must be between 5 and 15 characters long'),
+    body('formula').trim().isLength({ min: 3, max: 30 }).withMessage('formula must be between 3 and 30 characters long'),
+    body('description').trim().isLength({ min: 10 }).withMessage('description must be at least 10 characters long'),
+
+    async (req, res) => {
+        const stoneId = req.params.id;
+        const userId = req.user._id;
+        try {
+            const validation = validationResult(req);
+
+            if (validation.errors.length) {
+                throw validation.errors;
+            }
+
+            const result = await update(stoneId, req.body, userId);
+
+            res.redirect('/catalog/' + stoneId);
+
+        } catch (err) {
+            res.render('edit', { data: req.body, errors: parseError(err).errors });
+        }
+
+
+    }
+);
+
+stoneRouter.get('/delete/:id', isUser(), async (req, res) => {
+        const stoneId = req.params.id;
+        const userId = req.user._id;
+        try {
+            const validation = validationResult(req);
+
+            if (validation.errors.length) {
+                throw validation.errors;
+            }
+
+            const result = await update(stoneId, req.body, userId);
+
+            res.redirect('/catalog/' + stoneId);
+
+        } catch (err) {
+            res.render('edit', { data: req.body, errors: parseError(err).errors });
+        }
+
+
+    }
+);
 
 module.exports = { stoneRouter };

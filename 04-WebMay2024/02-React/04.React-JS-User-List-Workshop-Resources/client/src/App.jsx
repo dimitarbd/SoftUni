a1bfb3baca6aedea6be2ed4c218abce6f9199a81
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-07-07 22:10:28 +0300

    Update

---
+++ 04-WebMay2024/02-React/04.React-JS-User-List-Workshop-Resources/client/src/App.jsx
@@ -0,0 +1,22 @@
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+     <Header />
+
+     <main  className="main">
+
+    UserLi
+
+     </main>
+
+     <Footer />
+    </>
+  );
+}
+
+export default App;

c99bf7b7d7888e51f80d814b5f520c793dc13b23
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-07-16 23:27:00 +0300

    Update

--- 04-WebMay2024/02-React/04.React-JS-User-List-Workshop-Resources/client/src/components/user-section/UserSection.jsx
+++ 04-WebMay2024/02-React/04.React-JS-User-List-Workshop-Resources/client/src/components/user-section/UserSection.jsx
@@ -6,14 +6,15 @@ import UserList from './user-list/UserList';
 const baseUrl = 'http://localhost:3030/jsonstore';
 
 export default function UserSection(props) {
-    const [users, setUsers] = useState();
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         (async function getUsers() {
             const response = await fetch(`${baseUrl}/users`);
             const result = await response.json();
+            const users = Object.values(result);
 
-            console.log(result);
+            setUsers(users);
         })();
     }, []);
 

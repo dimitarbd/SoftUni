c3f9b8fb0aec66cc397ee6b630ea81d3adeeb0d4
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-07-28 12:27:52 +0300

    Create grid component

---
+++ 04-WebMay2024/02-React/EXAM-Project--E-Commerce/Client/src/components/Grid.jsx
@@ -0,0 +1,11 @@
+import GridBreadcrumb from './grid-breadcrumb/GridBreadcrumb';
+import GridProductSection from './grid-product-section/GridProductSection';
+
+export default function Grid() {
+  return (
+      <>
+      <GridBreadcrumb />
+      <GridProductSection />
+      </>
+  );
+}

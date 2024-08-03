aab7301608bb50a69edd7047c2294b3e64f9e61a
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-08-03 19:50:03 +0300

    Add global authstate and context

--- 04-WebMay2024/02-React/EXAM-Project--E-Commerce/Client/src/App.jsx
+++ 04-WebMay2024/02-React/EXAM-Project--E-Commerce/Client/src/App.jsx
@@ -4,19 +4,35 @@ import Home from './components/Home';
 import LoginRegister from './components/login-register/LoginRegister';
 import ShopDetails from './components/ShopDetails';
 import ShopCatalog from './components/ShopCatalog';
+import { useState } from 'react';
+import { AuthContext } from './contexts/AuthContext';
 
 
 function App() {
+  const [authState, setAuthstate] = useState({});
+
+  const changeAuthState = (state) => {
+    setAuthstate(state);
+  };
+
+  const contextData = {
+    email: authState.email,
+    accessToken: authState.accessToken,
+    isAuthenticated: !!authState.email,
+    changeAuthState,
+  };
 
   return (
     <>
+    <AuthContext.Provider value={contextData}>
     <Routes>
       <Route path='/' element={ <Home /> }/>
       <Route path='/catalog' element={ <ShopCatalog /> }/>
       <Route path='/details/:productId' element={ <ShopDetails /> }/>
       <Route path='/login' element={ <LoginRegister /> }/>
     </Routes>
-     
+
+    </AuthContext.Provider>     
     </>
   );
 }

47dc03ad066e03d3187b3ecf5329a40a026c07b8
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-09-29 14:26:58 +0300

    Update demo4.js

--- 04-WebMay2024/01-BackEnd/01-Streams&Utilities/02-Demo/demo4.js
+++ 04-WebMay2024/01-BackEnd/01-Streams&Utilities/02-Demo/demo4.js
@@ -1,8 +1,9 @@
-const { log } = require('console');
 const fs = require('fs');
 
-// const writer = fs.createWriteStream('./output.js');
+const writer = fs.createWriteStream('./output.js');
 
-process.stdin.on('data', (chunk) => {
-    console.log('>>>', chunk.toString()); 
-});
+// process.stdin.on('data', (chunk) => {
+//     writer.write(chunk.toString());
+// });
+
+process.stdin.pipe(writer);

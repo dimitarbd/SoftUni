be20ee9a11040801c9531ecc7e990cad3c529690
dimitarbd <146258599+dimitarbd@users.noreply.github.com>
2024-10-14 22:20:55 +0300

    Update main.ts

--- 05-TS-Okt2024/03-OOP/dist/main.js
+++ 05-TS-Okt2024/03-OOP/dist/main.js
@@ -7,3 +7,19 @@ const employee = {
     }
 };
 console.log(employee.getWage());
+class Person {
+    /**
+     *
+     */
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+const person1 = new Person('Ivan', 'Petrov');
+console.log(person1.getFullName());
+const person2 = new Person('Martin', 'Shopov');
+console.log(person2.getFullName());
--- 05-TS-Okt2024/03-OOP/src/main.ts
+++ 05-TS-Okt2024/03-OOP/src/main.ts
@@ -8,3 +8,24 @@ const employee = {
 };
 
 console.log(employee.getWage());
+
+class Person {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;        
+    } 
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+const person1 = new Person('Ivan', 'Petrov');
+console.log(person1.getFullName());
+
+const person2 = new Person('Martin', 'Shopov');
+console.log(person2.getFullName());
+
+

6adeb6df2213d5db5becb6c1fe21375fd5d256dc
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-11-26 21:57:36 +0200

    Create configHbs.js

--- 04-Web-May2024/01-BackEnd/RetakeExam-SKELETON2/src/config/configExpress.js
+++ 04-Web-May2024/01-BackEnd/RetakeExam-SKELETON2/src/config/configExpress.js
@@ -1,15 +1,7 @@
 const cookieParser = require('cookie-parser');
 const express = require('express')
-const handlebars = require('express-handlebars');
 
 function configExpress(app) {
-    const hbs = handlebars.create({
-        extname: 'hbs'
-    });
-
-    app.engine('hbs', hbs.engine);
-    app.set('view engine', 'hbs');
-
     app.use(cookieParser);
     //TODO add session middleware
 

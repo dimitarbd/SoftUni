3495c7285285615c43d9eafad924289c393c5daf
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-11-28 21:12:07 +0200

    Create home.js

--- 04-Web-May2024/01-BackEnd/RetakeExam-SKELETON2/src/controllers/home.js
+++ 04-Web-May2024/01-BackEnd/RetakeExam-SKELETON2/src/controllers/home.js
@@ -1,3 +1,11 @@
 const { Router } = require('express');
 
 const homeRouter = Router();
+
+homeRouter.get('/', (req, res) => {
+    console.log(req.user);
+    
+    res.render('home');
+})
+
+module.exports = { homeRouter };

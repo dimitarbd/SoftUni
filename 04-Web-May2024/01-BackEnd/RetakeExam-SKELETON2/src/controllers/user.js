24296534197a24899f9892c57ebe1a62d0a81145
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-11-29 22:24:19 +0200

    Update user.js

--- 04-Web-May2024/01-BackEnd/RetakeExam-SKELETON2/src/controllers/user.js
+++ 04-Web-May2024/01-BackEnd/RetakeExam-SKELETON2/src/controllers/user.js
@@ -3,3 +3,7 @@ const { Router } = require('express');
 const { isGuest } = require('../middlewares/guards');
 
 const userRouter = Router();
+
+userRouter.get('/register', isGuest(), (req, res) => {
+    res.render('register');
+})

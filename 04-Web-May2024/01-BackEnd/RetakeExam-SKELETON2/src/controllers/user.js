7a03dffafb0d203b37d4cedbac6d7714ab2dddcb
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-11-30 15:07:24 +0200

    Delete user.js

--- 04-Web-May2024/01-BackEnd/RetakeExam-SKELETON2/src/controllers/user.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const { isGuest } = require('../middlewares/guards');
-
-const userRouter = Router();
-
-userRouter.get('/register', isGuest(), (req, res) => {
-    res.render('register');
-})

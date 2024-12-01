b63643b2d571a384a75a6137e2b8023317b54cdf
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-12-01 10:24:23 +0200

    Create catalog.js

--- 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/config/configRoutes.js
+++ 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/config/configRoutes.js
@@ -1,9 +1,12 @@
 //TODO import routers
 
+const { catalogRouter } = require('../controllers/catalog');
 const { homeRouter } = require('../controllers/home');
 
 function configRoutes(app) {
     app.use(homeRouter);
+    app.use(catalogRouter);
+
     //TODO register routers
 }
 
--- 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/controllers/home.js
+++ 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/controllers/home.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
-const { createToken } = require('../services/jwt');
-const { login } = require('../services/user');
 
 const homeRouter = Router();
 
 homeRouter.get('/', async (req, res) => {
-    console.log(req.user);
-
     res.render('home');
+});
+
+homeRouter.get('/about', async (req, res) => {
+    res.render('about');
 });
 
 module.exports = { homeRouter };

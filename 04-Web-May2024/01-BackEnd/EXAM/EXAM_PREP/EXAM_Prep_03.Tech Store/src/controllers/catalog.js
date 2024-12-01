80e2ae4283d40c3251d2e6191899969208259813
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-12-01 23:51:43 +0200

    Update

--- 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/controllers/catalog.js
+++ 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/controllers/catalog.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getAll } = require('../services/techStore');
+const { getAll, getById } = require('../services/techStore');
 
 const catalogRouter = Router();
 
@@ -7,6 +7,21 @@ catalogRouter.get('/catalog', async (req, res) => {
     const products = await getAll();
 
     res.render('catalog', { products });
+})
+
+catalogRouter.get('/catalog/:id', async (req, res) => {
+    const id =  req.params.id;
+
+    const product = await getById(id);
+
+    if (!product) {
+        res.status(404).render('404')
+        return;
+    }
+
+    product.hasUser = res.locals.hasUser;
+
+    res.render('details', { product });
 })
 
 module.exports = { catalogRouter };

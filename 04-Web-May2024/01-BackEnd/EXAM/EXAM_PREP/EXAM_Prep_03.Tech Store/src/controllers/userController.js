d543cad66cce58e6d82ff78083ee8a8522c1683f
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-12-02 21:42:35 +0200

    Update userController.js

--- 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/controllers/userController.js
+++ 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/controllers/userController.js
@@ -1,22 +1,43 @@
 const { Router } = require('express');
+const { body, validationResult } = require('express-validator');
 
 const { createToken } = require('../services/jwt');
 const { login } = require('../services/user');
 const { isGuest } = require('../middlewares/guards');
-    
-    //This code creates a token and saves it in a cookie
-    // const result = await login('John', '123456');
-    // const token = createToken(result);
-    // res.cookie('token', token);
+const { parseError } = require('../util');
 
-    const userRouter = Router();
 
-    userRouter.get('/login', isGuest(), (req, res) => {
-        res.render('login');
-    })
+const userRouter = Router();
 
-    userRouter.get('/register', isGuest(), (req, res) => {
-        res.render('register');
-    })
+userRouter.get('/login', isGuest(), (req, res) => {
+    res.render('login');
+});
 
-    module.exports = { userRouter }
+userRouter.post('/login', isGuest(),
+    body('eamil').trim(),
+    body('password').trim(),
+    async (req, res) => {
+        const { email, password } = req.body;
+
+       try {
+            const result = await login( email, password);
+        const token = createToken(result);
+        res.cookie('token', token);
+
+        res.redirect('/');
+    } catch (err) {
+        res.render('login', { data: { email}, errors: parseError(err).errors })
+    }
+}
+);
+
+userRouter.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/');
+});
+
+userRouter.get('/register', isGuest(), (req, res) => {
+    res.render('register');
+})
+
+module.exports = { userRouter }

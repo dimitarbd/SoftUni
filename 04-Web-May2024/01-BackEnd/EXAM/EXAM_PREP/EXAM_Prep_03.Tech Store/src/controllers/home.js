7bd0fce44d2c6a82155bd0d704f068ea226e433f
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-12-05 23:00:40 +0200

    Update home.js

--- 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/controllers/home.js
+++ 04-Web-May2024/01-BackEnd/EXAM/EXAM_PREP/EXAM_Prep_03.Tech Store/src/controllers/home.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const { getRecent } = require('../services/techStore');
 
 const homeRouter = Router();
 
 homeRouter.get('/', async (req, res) => {
-    res.render('home');
+    const products = await getRecent();
+    res.render('home', { products });
 });
 
 homeRouter.get('/about', async (req, res) => {

8cc0d15ec3092e981b8fe7b40db2594ca1b9ef46
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-12-09 21:20:40 +0200

    Create courseBook.js

--- 04-Web-May2024/01-BackEnd/EXAM_PREP/EXAM_Prep_05_ CourseBook/src/models/CourseBook.js
+++ 04-Web-May2024/01-BackEnd/EXAM_PREP/EXAM_Prep_05_ CourseBook/src/models/CourseBook.js
@@ -3,11 +3,36 @@ const { Schema, model, Types } = require('mongoose');
 //TODO replace with data model from exam description
 
 const courseSchema = new Schema({
-    prop: {
+    title: {
         type: String,
         required: true,
     },
-    author: {
+    type: {
+        type: String,
+        required: true,
+    },
+    certificate: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    signUpList: {
+        type: [Types.ObjectId],
+        required: true,
+        default: []
+    },
+    owner: {
         type: Types.ObjectId,
         ref: 'User'
     }

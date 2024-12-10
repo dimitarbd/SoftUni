6889aadf73f927d0db048ed86e6d1d4d6b080f88
Dimitar Dimitrov <dimitarbd@abv.bg>
2024-12-10 22:41:29 +0200

    Create login.hbs

--- 04-Web-May2024/01-BackEnd/EXAM_PREP/EXAM_Prep_05_ CourseBook/src/config/configRoutes.js
+++ 04-Web-May2024/01-BackEnd/EXAM_PREP/EXAM_Prep_05_ CourseBook/src/config/configRoutes.js
@@ -1,9 +1,13 @@
 //TODO import routers
 
 const { homeRouter } = require('../controllers/home');
+const { userRouter } = require('../controllers/userController');
 
 function configRoutes(app) {
     app.use(homeRouter);
+    app.use(userRouter);
+
+    
     //TODO register routers
 }
 

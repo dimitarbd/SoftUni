70e98dede6a8231f1210808adc7390635c0485ba
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-02-02 21:04:59 +0200

    Update

--- 04-Web-May2024/02-React/06.Workshop-Components/client/src/App.jsx
+++ 04-Web-May2024/02-React/06.Workshop-Components/client/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import './styles.css'
 
@@ -6,6 +7,8 @@ function App() {
   return (
     <>
       <Header />
+      
+      <Footer />
     </>
   )
 }

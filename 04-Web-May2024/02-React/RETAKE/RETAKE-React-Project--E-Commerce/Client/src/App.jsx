aebb98e6568a93f76980a014826ba0929712a3cc
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-03 11:20:41 +0200

    Create Footer,jsx

--- 04-Web-May2024/02-React/RETAKE/RETAKE-React-Project--E-Commerce/Client/src/App.jsx
+++ 04-Web-May2024/02-React/RETAKE/RETAKE-React-Project--E-Commerce/Client/src/App.jsx
@@ -1,10 +1,15 @@
+import Footer from "./Components/footer/Footer"
+import Header from "./Components/header/Header"
+
 function App() {
 
     return (
         <body class="template-color-1">
 
             <div class="main-wrapper">
-                <h1>Car Spare Parts</h1>
+                <Header />
+
+                <Footer />
             </div>
 
         </body>

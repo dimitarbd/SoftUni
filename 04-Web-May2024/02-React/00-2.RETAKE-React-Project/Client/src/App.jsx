8e8d4382b3ac145e94be21b31ac8e475b19c4baa
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-12 23:09:02 +0200

    Update

--- 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/App.jsx
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/App.jsx
@@ -1,19 +1,11 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import Banner from "./Components/OLD/banner/Banner"
-import BannerTwo from "./Components/OLD/bannerTwo/BannerTwo"
-import BestSellingProduct from "./Components/OLD/bestSellingProduct/BestSellingProduct"
-import Blog from "./Components/OLD/blog/Blog"
-import Brand from "./Components/OLD/brand/Brand"
-import FeaturedCategories from "./Components/OLD/featuredCategories/FeaturedCategories"
 import Footer from "./Components/footer/Footer"
 import Header from "./Components/header/Header"
-import Modal from "./Components/OLD/modal/Modal"
 import Product from "./Components/product/Product"
 import Shipping from "./Components/shipping/Shipping"
 import Slider from "./Components/slider/Slider"
-import SpecialProduct from "./Components/OLD/specialProduct/SpecialProduct"
 
 function App() {
 
@@ -21,9 +13,9 @@ function App() {
         <>
             <Header />
 
-            <Slider />
+            {/* <Slider />
             <Shipping />
-            <Product />
+            <Product /> */}
 
             <Footer />
         </>
--- 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/Components/register/Register.jsx
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/Components/register/Register.jsx
@@ -18,38 +18,6 @@ export default function Register() {
     <div className="uren-login-register_area">
         <div className="container-fluid">
             <div className="row">
-                <div className="col-sm-12 col-md-12 col-xs-12 col-lg-6">
-                    {/* <!-- Login Form s--> */}
-                    <form action="#">
-                        <div className="login-form">
-                            <h4 className="login-title">Login</h4>
-                            <div className="row">
-                                <div className="col-md-12 col-12">
-                                    <label>Email Address*</label>
-                                    <input type="email" placeholder="Email Address"/>
-                                </div>
-                                <div className="col-12 mb--20">
-                                    <label>Password</label>
-                                    <input type="password" placeholder="Password"/>
-                                </div>
-                                <div className="col-md-8">
-                                    <div className="check-box">
-                                        <input type="checkbox" id="remember_me"/>
-                                        <label for="remember_me">Remember me</label>
-                                    </div>
-                                </div>
-                                <div className="col-md-4">
-                                    <div className="forgotton-password_info">
-                                        <a href="#"> Forgotten pasward?</a>
-                                    </div>
-                                </div>
-                                <div className="col-md-12">
-                                    <button className="uren-login_btn">Login</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
                 <div className="col-sm-12 col-md-12 col-lg-6 col-xs-12">
                     <form action="#">
                         <div className="login-form">

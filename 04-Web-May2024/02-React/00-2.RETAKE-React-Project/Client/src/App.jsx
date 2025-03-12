5f6e4cd7aa4b89469acd32c3109a053db45dcd37
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-12 21:24:29 +0200

    Tidying up and cleaning

--- 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/App.jsx
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/App.jsx
@@ -1,45 +1,33 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import Banner from "./Components/banner/Banner"
-import BannerTwo from "./Components/bannerTwo/BannerTwo"
-import BestSellingProduct from "./Components/bestSellingProduct/BestSellingProduct"
-import Blog from "./Components/blog/Blog"
-import Brand from "./Components/brand/Brand"
-import FeaturedCategories from "./Components/featuredCategories/FeaturedCategories"
+import Banner from "./Components/OLD/banner/Banner"
+import BannerTwo from "./Components/OLD/bannerTwo/BannerTwo"
+import BestSellingProduct from "./Components/OLD/bestSellingProduct/BestSellingProduct"
+import Blog from "./Components/OLD/blog/Blog"
+import Brand from "./Components/OLD/brand/Brand"
+import FeaturedCategories from "./Components/OLD/featuredCategories/FeaturedCategories"
 import Footer from "./Components/footer/Footer"
 import Header from "./Components/header/Header"
-import Modal from "./Components/modal/Modal"
+import Modal from "./Components/OLD/modal/Modal"
 import Product from "./Components/product/Product"
 import Shipping from "./Components/shipping/Shipping"
 import Slider from "./Components/slider/Slider"
-import SpecialProduct from "./Components/specialProduct/SpecialProduct"
+import SpecialProduct from "./Components/OLD/specialProduct/SpecialProduct"
 
 function App() {
 
     return (
-        // <body class="template-color-1">
-<>
-{/* // <div class="main-wrapper"> */}
-    <Header />
+        <>
+            <Header />
 
-    <Slider />
-    <Shipping />
-    {/* <SpecialProduct />  //TODO delete SpecialProduct */}
-    {/* <Banner /> //TODO delete */}
-    <Product /> 
-    {/* <BannerTwo /> //TODO delete */}
-    {/* <BestSellingProduct /> //TODO delete */}
-    {/* <FeaturedCategories /> //TODO delete */}
-    {/* <Brand /> //TODO delete */}
-    {/* <Blog /> //TODO delete */}
-    {/* <Modal /> //TODO delete*/}
-    
-    <Footer />
-{/* // </div> */}
-</>
+            <Slider />
+            <Shipping />
+            <Product />
 
-        // </body>
+            <Footer />
+        </>
+
 
     )
 }

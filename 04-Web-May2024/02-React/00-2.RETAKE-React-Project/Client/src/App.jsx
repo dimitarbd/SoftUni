7ca8d1d54f287949d966058e10e53c7dd4d75116
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-13 21:46:22 +0200

    Add routes in App.jsx

--- 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/App.jsx
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/App.jsx
@@ -1,28 +1,30 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+import { Routes, Route } from 'react-router-dom';
+
 import Footer from "./Components/footer/Footer"
 import Header from "./Components/header/Header"
-import Product from "./Components/product/Product"
-import Shipping from "./Components/shipping/Shipping"
-import Slider from "./Components/slider/Slider"
 import Register from "./Components/register/Register"
 import Login from "./Components/login/Login"
+import Home from "./Home"
 function App() {
 
     return (
-        <>
+        <div className="main-wrapper">
+
             <Header />
 
-            <Slider />
-            <Shipping />
-            <Product /> 
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
 
             {/* <Register /> */}
             {/* <Login /> */}
 
             <Footer />
-        </>
+
+        </div>
 
 
     )
---
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/Home.jsx
@@ -0,0 +1,13 @@
+import Slider from "./Components/slider/Slider"
+import Shipping from "./Components/shipping/Shipping"
+import Product from "./Components/product/Product"
+
+export default function Home() {
+    return (
+        <>
+            <Slider />
+            <Shipping />
+            <Product />
+        </>
+    );
+}

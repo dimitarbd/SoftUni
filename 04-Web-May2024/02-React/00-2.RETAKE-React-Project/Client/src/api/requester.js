5415af0c511b415a9ec100433ee7235594193415
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-15 22:09:14 +0200

    Update requester.js

--- 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/api/requester.js
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/api/requester.js
@@ -1,3 +1,17 @@
-export default function requester(method, url, data) {
-    
-}
+export default async function requester(method, url, data) {
+    const options = {};
+
+    if (method !== 'GET'){
+        options.method = method;
+    }
+
+    if (data) {
+        options.headers = {
+            'Content-Type': 'application/json'
+        }
+
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, options);
+};

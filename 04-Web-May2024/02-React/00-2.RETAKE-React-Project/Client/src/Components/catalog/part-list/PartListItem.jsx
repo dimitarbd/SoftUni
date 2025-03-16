6d47a30f07e67eefc3c6586d423132dcdf41e9ae
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-16 22:46:18 +0200

    Implement rating functionality

--- 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/Components/catalog/part-list/PartListItem.jsx
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/Components/catalog/part-list/PartListItem.jsx
@@ -1,33 +1,44 @@
 import { Link } from "react-router-dom";
 
-export default function PartListItem() {
+export default function PartListItem({
+    _id,
+    title,
+    price,
+    imageUrl,
+    rating
+}) {
+    const renderRating = (rating) => {
+        const stars = [];
+        for (let i = 1; i <= 5; i++) {
+            stars.push(
+                <li key={i} className={i <= rating ? "" : "silver-color"}>
+                    <i className="ion-android-star"></i>
+                </li>
+            );
+        }
+        return stars;
+    };
+
     return (
         <div className="col-lg-4">
             <div className="product-slide_item">
                 <div className="inner-slide">
                     <div className="single-product">
                         <div className="product-img">
-                            <Link to="single-product.html">
-                                <img className="primary-img" src="/images/product/large-size/1.jpg" alt="Uren's Product Image" />
+                            <Link to="#">
+                                <img className="primary-img" src={imageUrl} alt="Uren's Product Image" />
                             </Link>
                         </div>
                         <div className="product-content">
                             <div className="product-desc_info">
                                 <div className="rating-box">
                                     <ul>
-                                        <li><i className="ion-android-star"></i></li>
-                                        <li><i className="ion-android-star"></i></li>
-                                        <li><i className="ion-android-star"></i></li>
-                                        <li className="silver-color"><i className="ion-android-star"></i>
-                                        </li>
-                                        <li className="silver-color"><i className="ion-android-star"></i>
-                                        </li>
+                                        {renderRating(rating)}
                                     </ul>
                                 </div>
-                                <h6><Link className="product-name" to="single-product.html">Veniam
-                                    officiis voluptates</Link></h6>
+                                <h6><Link className="product-name" to="single-product.html">{title}</Link></h6>
                                 <div className="price-box">
-                                    <span className="new-price">$122.00</span>
+                                    <span className="new-price">${price}</span>
                                 </div>
                             </div>
                         </div>

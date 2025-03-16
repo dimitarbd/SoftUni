ac7b646f205923bc85039640ca59c1ffeb271924
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-16 12:23:34 +0200

    Update Details.jsx

--- 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/api/parts-api.js
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/api/parts-api.js
@@ -1,5 +1,5 @@
 import * as requester from './requester';
 
-const BASE_URL = 'http://localhost:3030/jsonstore/games'
+const BASE_URL = 'http://localhost:3030/jsonstore/car-parts'
 
 export const getAll = () => requester.get(BASE_URL);

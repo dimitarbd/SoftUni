6f1ea1187fa8ca8e5deb18586d94b1fe22c5e3ca
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-20 23:22:45 +0200

    Update comment-api.js

--- 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/api/comments-api.js
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/api/comments-api.js
@@ -1,1 +1,13 @@
-const create = (username)
+import requester from "./requester";
+
+const BASE_URL = 'http://localhost:3030/jsonstore/car-parts';
+
+const buildUrl = (partId) => `${BASE_URL}/${partId}/comments`;
+
+const create = (partId, username, text) => {
+    const result = requester.post(buildUrl(partId), { username, text });
+
+    const comments = Object.values(result);
+
+    return comments;
+};

7ed56df9c48bdc4c06584a6e10c8551c40cf14d2
Dimitar Dimitrov <dimitarbd@abv.bg>
2025-03-23 10:59:27 +0200

    Create Login and Register API

---
+++ 04-Web-May2024/02-React/00-2.RETAKE-React-Project/Client/src/api/auth-api.js
@@ -0,0 +1,7 @@
+import requester from "./requester";
+
+const BASE_URL = 'http://localhost:3030/users';
+
+export const login = async (email, password) => requester.post(`${BASE_URL}/login`, { email, password });
+
+export const register = async (email, password) => requester.post(`${BASE_URL}/register`, { email, password });
